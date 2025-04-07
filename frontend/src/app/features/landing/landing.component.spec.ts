a14f63bbe0d1e631b603fe88ed35d7990376d4d4
EmersonBarranco <emersonbarranco@wposs.com>
2025-04-07 15:47:01 -0500

    cambios

--- frontend/src/app/features/landing/landing.component.spec.ts
+++ frontend/src/app/features/landing/landing.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LandingComponent } from './landing.component';
+import LandingComponent from './landing.component';
 
 describe('LandingComponent', () => {
   let component: LandingComponent;
@@ -8,10 +8,9 @@ describe('LandingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LandingComponent]
-    })
-    .compileComponents();
-    
+      imports: [LandingComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LandingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
