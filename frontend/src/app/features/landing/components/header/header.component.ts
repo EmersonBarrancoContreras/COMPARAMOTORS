b96097a7ecb7ba984bd790894822692c722401b5
EmersonBarranco <emersonbarranco@wposs.com>
2025-04-07 17:02:05 -0500

    se completo la configuración de la update a angular 19 y los themes de primeng

--- frontend/src/app/features/landing/components/header/header.component.ts
+++ frontend/src/app/features/landing/components/header/header.component.ts
@@ -1,15 +1,25 @@
+import { Toast } from 'primeng/toast';
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    ButtonModule
-  ],
+  imports: [ButtonModule, Toast],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  providers: [MessageService],
 })
 export class HeaderComponent {
+  title = 'primeng';
+  constructor(private MessageService: MessageService) {}
 
+  show() {
+    this.MessageService.add({
+      severity: 'success',
+      summary: 'Info Message',
+      detail: 'PrimeNG Toast',
+    });
+  }
 }
