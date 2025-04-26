d9633ff072cd37eba5c6a1941f5bb99fdd0b4d38
EmersonBarranco <emersonbarranco@wposs.com>
2025-04-26 12:44:53 -0500

    mejoramiento del componente de sections

--- frontend/src/app/features/landing/components/header/header.component.ts
+++ frontend/src/app/features/landing/components/header/header.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { MenuItem } from 'primeng/api';
-import { Menu } from 'primeng/menu';
+import { MenuModule } from 'primeng/menu';
+import { InputIconModule } from 'primeng/inputicon';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputTextModule } from 'primeng/inputtext';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ButtonModule, Menu],
+  imports: [
+    ButtonModule,
+    MenuModule,
+    InputIconModule,
+    IconFieldModule,
+    InputTextModule,
+    MenubarModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   providers: [],
