793cef03140a7308f3d12d9a28c60a3cba4dca0d
EmersonBarranco <emersonbarranco@wposs.com>
2025-05-09 16:57:57 -0500

    modificación en el componente slider

--- frontend/src/app/features/landing/components/seccion-1/seccion-1.component.ts
+++ frontend/src/app/features/landing/components/seccion-1/seccion-1.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-seccion-1',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './seccion-1.component.html',
-  styleUrl: './seccion-1.component.scss'
+  styleUrl: './seccion-1.component.scss',
 })
-export class Seccion1Component {
+export class Seccion1Component implements OnInit {
+  expandedIndex: number | null = null;
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onClick() {
+    const element = document.getElementById('img');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  toggleItem(index: number, event: MouseEvent) {
+    event.stopPropagation(); // Evitar que el clic se propague a la tarjeta
+    if (this.expandedIndex === index) {
+      this.expandedIndex = null; // Contraer si ya está expandido
+    } else {
+      this.expandedIndex = index; // Expandir el ítem seleccionado
+    }
+  }
 }
