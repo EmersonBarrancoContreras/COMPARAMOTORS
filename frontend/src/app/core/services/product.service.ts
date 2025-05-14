f915335bfdd80add5d80c3e43e6ad4c2b4994e19
EmersonBarranco <emersonbarranco@wposs.com>
2025-05-14 16:52:15 -0500

    cambios en el header

--- frontend/src/app/core/services/product.service.ts
+++ frontend/src/app/core/services/product.service.ts
@@ -21,8 +21,7 @@ export class ProductService {
       {
         id: '1000',
         name: 'LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISICING ELIT SUSCIPIT QUIS',
-        description:
-          'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum',
+        description: '',
         price: 65,
         quantity: 24,
         category: 'Accessories',
